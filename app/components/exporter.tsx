e4f71fe6b81dbf2d3a6ac79331216051d4c0a406
mcyin <724651441@qq.com>
2026-04-18 12:47:51 +0800

    docs(exporter): 更新预览界面中的品牌名称和仓库链接

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -524,9 +524,9 @@ export function ImagePreviewer(props: {
           </div>
 
           <div>
-            <div className={styles["main-title"]}>NextChat</div>
+            <div className={styles["main-title"]}>小殷Chat</div>
             <div className={styles["sub-title"]}>
-              github.com/ChatGPTNextWeb/ChatGPT-Next-Web
+              github.com/mcyin-fork/mcyin-chat
             </div>
             <div className={styles["icons"]}>
               <MaskAvatar avatar={config.avatar} />
